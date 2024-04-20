1c46aee06aed387068162e82f99ed15dbe036288
Odhi-ambo <michaelderrick605@gmail.com>
2024-04-20 12:44:44 +0300

    initial commit starting the build

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,30 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app_fl/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key:key);
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(children: [
-            Icon(
-              Icons.menu,
-              color: tdBlack, 
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                child: Image.asset('assets/images/pngtree-vector-avatar-icon-png-image_695765.jpg'),
-              ),
-            )
-        ]),
-      ),
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is the HomeScreen'),
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        
+          child: Column(children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: tdBlack,
+                    size: 20,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20,
+                    minHeight: 25,
+
+                  ),
+                  border: InputBorder.none,
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: tdGrey),
+
+                ),
+              ),
+              ),
+          ],
+          ),
+        ),
+      
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/pngtree-vector-avatar-icon-png-image_695765.jpg'),
+            ),
+          ),
+        ],
       ),
     );
   }
