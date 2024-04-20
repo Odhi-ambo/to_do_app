78c41c17cf78ab053587c179efd1169d8317912f
Odhi-ambo <michaelderrick605@gmail.com>
2024-04-20 11:24:10 +0300

    modified-menu icons

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app_fl/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key:key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todo App'),),
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack, 
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                child: Image.asset('assets/images/pngtree-vector-avatar-icon-png-image_695765.jpg'),
+              ),
+            )
+        ]),
+      ),
       body: Container(
         child: Text('This is the HomeScreen'),
       ),
